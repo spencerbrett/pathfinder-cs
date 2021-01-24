f0c0b3dcda7e4b75d90b38c87a29e5b25005507d
spencer <spencer.a.brett@gmail.com>
2021-01-23 17:27:04 -0800

    Use HashRouter

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter baseline="/">
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
